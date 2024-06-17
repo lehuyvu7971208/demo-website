4f6fc69bf4341862dacd02aa6d4b1017660d0b2d
Lê Huy Vũ <lehuyvu7971208@gmail.com>
2024-06-17 15:24:52 +0700

    Optimize way to close the login modal

---
+++ src/app/(pages)/posts/@auth/page.tsx
@@ -0,0 +1,3 @@
+export default async function CatchAllPage() {
+  return null;
+}
--- src/app/(pages)/posts/layout.tsx
+++ src/app/(pages)/posts/layout.tsx
@@ -5,19 +5,12 @@ import { PropsWithChildren, ReactNode, useMemo } from "react";
 
 // Components
 import Header from "./_components/header";
-import { usePathname } from "next/navigation";
 
 type PageLayoutProps = PropsWithChildren & {
   auth: ReactNode;
 };
 
 export default function PageLayout({ auth, children }: PageLayoutProps) {
-  const pathname = usePathname();
-
-  const isAuthShown = useMemo(() => {
-    return ["/auth/sign-in", "/auth/sign-up"].includes(pathname);
-  }, [pathname]);
-
   return (
     <div className="container mx-auto px-2 py-3 flex flex-col gap-y-3">
       <div className="flex-0">
@@ -26,7 +19,7 @@ export default function PageLayout({ auth, children }: PageLayoutProps) {
 
       <div className="flex-0">{children}</div>
 
-      {isAuthShown && auth}
+      {auth}
     </div>
   );
 }
